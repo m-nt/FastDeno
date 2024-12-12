8632c2ee85621446edcc557edb6ca5a655368d0b
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2024-12-12 14:10:32 +0330

    Feat: init project

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { FastDeno, FastRequest } from "./src/server.ts";
+
+const server = new FastDeno();
+
+server.add_route("GET","/",(req?: FastRequest)=>{
+    console.log(req);
+    console.log(req?.body);
+    
+    return {message:"hi"}
+})
+server.add_route("GET","/mamad",(req,res,next)=>{
+    console.log("MIDDLEWARE");
+    res.status = 300
+    next()
+},
+    (req,res)=>{
+    console.log("ROUTE");
+    res?.headers.set("MAMAD","NABODI")
+    res.send(200,{message:"mamad"})
+})
+await server.listen(6969, "0.0.0.0");
